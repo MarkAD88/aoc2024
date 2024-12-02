99ca8f7b73ef764a52a985197aaa450d30c30012
Mark Dellacca <markad88@hotmail.com>
2024-12-02 11:02:46 -0800

    Day 1 parts 1 & 2

---
+++ 01_historian_hysteria/Program.cs
@@ -0,0 +1,43 @@
+﻿List<int> left = [];
+List<int> right = [];
+
+// PART 1
+
+using (var input = File.OpenText("input.txt"))
+{
+    string? line = null;
+    while ((line = input.ReadLine()) != null)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            continue;
+        }
+
+        var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        left.Add(int.Parse(parts[0]));
+        right.Add(int.Parse(parts[1]));
+    }
+}
+
+left.Sort();
+right.Sort();
+
+long diff = 0;
+for (int i = 0; i < right.Count; i++)
+{
+    diff += Math.Abs(right[i] - left[i]);
+}
+
+Console.WriteLine(diff);
+
+// PART 2
+
+long similarity = 0;
+for (int i = 0; i < left.Count; i++)
+{
+    int value = left[i];
+    int occurances = right.Count(x => x == value);
+    similarity += (value * occurances);
+}
+
+Console.WriteLine(similarity);
---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent of Code 2024
+
+C# implementations and tests using .NET 9 for the
+[Advent of Code 2024](https://adventofcode.com/2024/) event.
+
+```text
+      -----Part 1-----  -----Part 2-----
+Day   HH:MM    Success  HH:MM    Success
+  1   00:12      Yes    00:04      Yes
+```
