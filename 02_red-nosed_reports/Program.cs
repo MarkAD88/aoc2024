422a9d75c63850e15315f8182a8f6c2a9637e4a4
Mark Dellacca <markad88@hotmail.com>
2024-12-02 17:36:22 -0800

    Day 2 part 2

--- 02_red-nosed_reports/Program.cs
+++ 02_red-nosed_reports/Program.cs
@@ -1,7 +1,37 @@
-﻿List<List<int>> reports = [];
+﻿static bool IsSafeReport(List<int> report, bool allowError = false)
+{
+    int lastDiff = report[0] - report[1];
+    for (int i = 1; i < report.Count; i++)
+    {
+        int diff = report[i - 1] - report[i];
+        if (Math.Abs(diff) is 0 or > 3
+            || (lastDiff < 0 && diff > 0)
+            || (lastDiff > 0 && diff < 0))
+        {
+            if (allowError)
+            {
+                for (int j = 0; j < report.Count; j++)
+                {
+                    List<int> test = [.. report];
+                    test.RemoveAt(j);
+                    if (IsSafeReport(test))
+                    {
+                        return true;
+                    }
+                }
+            }
 
-/* Testing (should produce 2)
-reports = [
+            return false;
+        }
+
+        lastDiff = diff;
+    }
+
+    return true;
+}
+
+/* Testing (should produce 2) */
+List<List<int>> reports = [
     [7, 6, 4, 2, 1],
     [1, 2, 7, 8, 9],
     [9, 7, 6, 2, 1],
@@ -9,10 +39,10 @@ reports = [
     [8, 6, 4, 4, 1],
     [1, 3, 6, 7, 9],
 ];
-*/
 
 using (var input = File.OpenText("input.txt"))
 {
+    reports.Clear();
     string? line = null;
     while ((line = input.ReadLine()) != null)
     {
@@ -27,50 +57,27 @@ using (var input = File.OpenText("input.txt"))
 }
 
 int safeReports = 0;
+;
+// Step 1
 foreach (var report in reports)
 {
-    // Determine if increasing or decreasing by comparing first two values
-
-    // Check directional compliance
-    List<int> sorted = [];
-    if (report[0] == report[1])
+    if (IsSafeReport(report))
     {
-        // Unsafe
-        continue;
+        safeReports++;
     }
-    else if (report[0] < report[1])
-    {
-        sorted = [.. report.OrderBy(x => x)];
-    }
-    else
-    {
-        sorted = [.. report.OrderByDescending(x => x)];
-    }
-
-    // If the values don't flow in the proper direction
-    // bail out - unsafe
-    if (!sorted.SequenceEqual(report))
-    {
-        continue;
-    }
-
-    // If the values are not within a safe incrementing range the
-    // report is unsafe
-    bool safe = true;
-    for (int index = 1; index < report.Count; index++)
-    {
-        if (Math.Abs(report[index - 1] - report[index]) is 0 or > 3)
-        {
-            safe = false;
-            break;
-        }
-    }
-    if (!safe)
-    {
-        continue;
-    }
-
-    safeReports++;
 }
+Console.WriteLine($"Safe Reports (part 1): {safeReports}");
+Console.WriteLine();
 
-Console.WriteLine($"Safe Reports: {safeReports}");
+// Step 2
+safeReports = 0;
+int index = 0;
+foreach (var report in reports)
+{
+    if (IsSafeReport(report, true))
+    {
+        safeReports++;
+    }
+    index++;
+}
+Console.WriteLine($"Safe Reports (part 2): {safeReports}");
--- README.md
+++ README.md
@@ -6,6 +6,6 @@ C# implementations and tests using .NET 9 for the
 ```text
       -----Part 1-----  -----Part 2-----
 Day   HH:MM    Success  HH:MM    Success
-  2   00:22      Yes
+  2   00:22      Yes    00:45      Yes
   1   00:12      Yes    00:04      Yes
 ```
